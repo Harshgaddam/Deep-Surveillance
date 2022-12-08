4326e82b54cac6a1911a2abb76f05a07cb92f4d6
unknown <harshvardhangaddam@gmail.com>
2022-12-08 17:05:42 +0530

    Initial commit

---
+++ Rough.py
@@ -0,0 +1,5 @@
+import os
+# os.system('ffmpeg -i ./train/training_videos/01.avi -r 0.2 train/frames/image.jpg')
+
+os.system('ffmpeg -i train/training_videos/01.avi -r 0.2 train/frames/image.jpg')
+# C:\Users\G Harsh Vardhan\Desktop\pr\train\training_videos
